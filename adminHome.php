b0e6327a84df5072069c194314469042077dfad1
Andrew <forrestandrew7@gmail.com>
2022-04-25 16:24:41 +0100

    edit features

--- adminHome.php
+++ adminHome.php
@@ -29,43 +29,67 @@ if ( !isset( $_SESSION[ 'admin_id' ] ) ) { require ( 'admin_login_tools.php' ) ;
 # Set page title and display header section.
 
 //$page_title = 'Home' ;
-include ( 'logout.html' ) ;
+include ( 'admin.html' ) ;
 
-# Open database connection.
-	require ( 'connect1.php' ) ;
+echo '
 
-        
-# Retrieve movies from 'movie' database table.
-	$q = "SELECT * FROM movie" ;
-	$r = mysqli_query( $link, $q ) ;
-	if ( mysqli_num_rows( $r ) > 0 )
-	{
-         # Display body section.
-	while ( $row = mysqli_fetch_array( $r, MYSQLI_ASSOC ))
-	{
-	echo '  
-                           <div class="col-md-3 d-flex justify-content-center">
-		 <div class="card text-center" style="width: 18rem;">
-		<div class="card text-center">	  
-				
-				  <img src='. $row['img'].' alt="Movie" class="img-thumbnail bg-secondary">
-				  <h5 class="card-title">'. $row['movie_title'].' </h5>
-				  <a href="movie.php?id='.$row['id'].'" class="btn btn-secondary btn-block" role="button">
-				 Watch Now</a>
-			   </div>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+		<div class="card bg-light mb-3">
+		  <div class="card-header">Register as Admin</div>
+		  <div class="card-body">
+			<h5 class="card-title">Register as Admin</h5>
+			<p class="card-text">Send a request to gain access to admin portal</p>
+			<a href="adminRegister.php" class="btn btn-secondary btn-lg btn-block">Register</a>
 		  </div>
-		</div>';
-                
+		</div>
+	</div>
+	<div class="col-sm">
+		<div class="card bg-light mb-3">
+		  <div class="card-header">Admin</div>
+		  <div class="card-body">
+			<h5 class="card-title">Admin</h5>
+			<p class="card-text">Log in with your admin details to proceed</p>
+			<a href="adminLogin.php" class="btn btn-secondary btn-lg btn-block" data-toggle="modal" data-target="#exampleModal">Sign In</a>
+            <br>
+            <a href="admin_forgotPassword.php">Forgotten password?</a>
+		  </div>
+		</div>
+	</div>
+	</div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Sign In</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       
+		<form action="admin_login_action.php" class="was-validated" method="post"method="post">
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Email" name="email" required>
+			</div>
+			<div class="form-group">
+				<input type="password" class="form-control" placeholder="Password" name="pass" required>
+			</div>
+				
 
-        
-	}
-	# Close database connection.
-	mysqli_close( $link) ; 
-	}
-        
-        # Or display message.
-	else { echo '<p>There are currently no movies showing.</p>
-	' ; }
+      </div>
+      <div class="modal-footer">
+        <input type="submit" class="btn btn-secondary btn-lg btn-block" value="Sign In" >
+		</form>
+      </div>
+    </div>
+  </div>
+</div>
+
+';
         
         
 # Display footer section.
