4ad2e4a657a5669f92bd0727669632b8ae51e4f7
Andrew <forrestandrew7@gmail.com>
2022-04-30 10:18:06 +0100

    Logout

--- adminLogout.php
+++ adminLogout.php
@@ -4,7 +4,7 @@
 session_start() ;
 
 # Redirect if not logged in.
-if ( !isset( $_SESSION[ 'user_id' ] ) ) { require ( 'login_tools.php' ) ; load() ; }
+if ( !isset( $_SESSION[ 'admin_id' ] ) ) { require ( 'admin_login_tools.php' ) ; load() ; }
 
 # Set page title and display header section.
 $page_title = 'Home' ;
@@ -24,4 +24,3 @@ echo '<h1>Goodbye!</h1><p>You are now logged out.</p><p><a href="index.php">Logi
 # Display footer section.
 include ( 'footer.html' ) ;
 ?>
-
--- review.php
+++ review.php
@@ -13,6 +13,7 @@ session_start() ;
 
 # Redirect if not logged in.
 if ( !isset( $_SESSION[ 'user_id' ] ) ) { require ( 'login_tools.php' ) ; load() ; }
+echo "{$_SESSION['user_id']}";
 
 # Set page title and display header section.
 $page_title = 'Reviews' ;
