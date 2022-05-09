654b2e3264d6ee193e732800decf0be308a5298b
Andrew <forrestandrew7@gmail.com>
2022-05-09 17:19:12 +0100

    updates

--- AboutUs.php
+++ AboutUs.php
@@ -14,11 +14,12 @@ echo '<div class="container">
 
 		<div class="card-deck">
 		  <div class="card">
-			<img src="img/family.jpg" class="card-img-top" alt="4DX Cinema Experience">
+			<img src="img/TVfamily.jpeg" class="card-img-top" alt="4DX Cinema Experience">
 			<div class="card-body">
 			  <h5 class="card-title">What is Edmax</h5>
-			  <p class="card-text">We are a small family owned cinema located in Edinburgh!</p>
-                          <p class="card-text">Please register an account with us to explore all of the great movies we have available, book tickets and see what food and drink we serve!</p>
+			  <p class="card-text">We are a small family owned streaming service located in Edinburgh!</p>
+                          <p class="card-text">Please register an account with us 
+						  to explore all of the great movies and TV shows we have available!</p>
 			  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 			</div>
 		  </div>
